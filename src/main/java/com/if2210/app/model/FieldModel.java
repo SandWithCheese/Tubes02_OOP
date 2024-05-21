1c4fed9fb88a24433e520dfcaf415ed7cfea0e87
rifchzschki <114592995+rifchzschki@users.noreply.github.com>
2024-05-21 21:47:59 +0700

    feat: field model

--- src/main/java/com/if2210/app/model/FieldModel.java
+++ src/main/java/com/if2210/app/model/FieldModel.java
@@ -1,5 +1,17 @@
 package com.if2210.app.model;
 
 public class FieldModel {
-    
+    private CardModel[][] field;
+
+    public FieldModel(){
+        field = new CardModel[4][5];
+    }
+
+    public CardModel[][] getField() {
+        return field;
+    }
+
+    public void setField(CardModel[][] field) {
+        this.field = field;
+    }
 }
