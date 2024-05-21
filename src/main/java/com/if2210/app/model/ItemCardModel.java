412301e98af33c782e50b2143f39a8c5edeb0f01
rifchzschki <114592995+rifchzschki@users.noreply.github.com>
2024-05-21 16:23:17 +0700

    feat: card, shop, field

---
+++ src/main/java/com/if2210/app/model/ItemCardModel.java
@@ -0,0 +1,5 @@
+package com.if2210.app.model;
+
+public class ItemCardModel extends Card {
+    
+}
---
+++ src/main/java/com/if2210/app/model/ShopModel.java
@@ -0,0 +1,18 @@
+package com.if2210.app.model;
+import java.util.Map;
+import java.util.HashMap;
+import java.lang.Exception;
+
+public class ShopModel{
+    private Map<Card, Integer> daftarProduk = new HashMap<>();
+
+    // method untuk menambahkan produk ke dalam shop
+    public void addProduk(ProductCardModel a) throws Exception{
+
+    }
+
+    // method untuk mendapatkan produk lalu menguranginya di daftar produk
+    public void getProduk() throws Exception{
+
+    }
+}
