183ada5d1abd54df1ef0466b7e811e4edcbd32d5
SandWithCheese <naufalahmad022@gmail.com>
2024-05-22 20:44:02 +0700

    feat: connect gui fxml to game controller

--- src/main/java/com/if2210/app/model/GameManagerModel.java
+++ src/main/java/com/if2210/app/model/GameManagerModel.java
@@ -11,6 +11,8 @@ import com.if2210.app.datastore.DataManager;
 public class GameManagerModel {
     private DataManager dataManager;
     private int currentTurn;
+    // Penentu giliran pemain, cuma bisa 0 atau 1
+    private int whoseTurn;
     private ShopModel shop;
     private PlayerModel player1;
     private PlayerModel player2;
@@ -28,20 +30,57 @@ public class GameManagerModel {
         this.shop = ShopModel.getInstance();
 
         this.currentTurn = this.dataManager.getCurrentTurn();
+        this.whoseTurn = 0;
         this.player1 = this.dataManager.getPlayer1();
         this.player2 = this.dataManager.getPlayer2();
         this.shop.setProductList(this.dataManager.getProductList());
+    }
 
-        // Print all data
-        System.out.println("Current turn: " + this.currentTurn);
-        System.out.println("Player 1 field: " + this.player1.getField());
-        System.out.println("Player 1 active deck: " + this.player1.getActiveDeck());
-        System.out.println("Player 1 deck: " + this.player1.getDeck());
-        System.out.println("Player 1 money: " + this.player1.getMoney());
-        System.out.println("Player 2 field: " + this.player2.getField());
-        System.out.println("Player 2 active deck: " + this.player2.getActiveDeck());
-        System.out.println("Player 2 deck: " + this.player2.getDeck());
-        System.out.println("Player 2 money: " + this.player2.getMoney());
-        System.out.println("Shop product list: " + this.shop.getProductList());
+    public DataManager getDataManager() {
+        return dataManager;
+    }
+
+    public void setDataManager(DataManager dataManager) {
+        this.dataManager = dataManager;
+    }
+
+    public int getCurrentTurn() {
+        return currentTurn;
+    }
+
+    public void setCurrentTurn(int currentTurn) {
+        this.currentTurn = currentTurn;
+    }
+
+    public int getWhoseTurn() {
+        return whoseTurn;
+    }
+
+    public void setWhoseTurn(int whoseTurn) {
+        this.whoseTurn = whoseTurn;
+    }
+
+    public ShopModel getShop() {
+        return shop;
+    }
+
+    public void setShop(ShopModel shop) {
+        this.shop = shop;
+    }
+
+    public PlayerModel getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(PlayerModel player1) {
+        this.player1 = player1;
+    }
+
+    public PlayerModel getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(PlayerModel player2) {
+        this.player2 = player2;
     }
 }
