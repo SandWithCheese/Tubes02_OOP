ea2e52a0312a418920a930f13770a0d5fe40fc9d
SandWithCheese <naufalahmad022@gmail.com>
2024-05-23 22:06:15 +0700

    feat: add song

--- src/main/java/com/if2210/app/App.java
+++ src/main/java/com/if2210/app/App.java
@@ -1,5 +1,12 @@
 package com.if2210.app;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -7,6 +14,10 @@ import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
 public class App extends Application {
+    private List<Media> songs = new ArrayList<>();
+    private int currentSongIndex = 0;
+    private MediaPlayer mediaPlayer;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -21,6 +32,22 @@ public class App extends Application {
         // Load the logo image for taskbar logo
         String iconPath = "/com/if2210/app/assets/Anya.png";
         stage.getIcons().add(new javafx.scene.image.Image(iconPath));
+
+        // Add song to the scene
+        String songPath1 = "/com/if2210/app/music/daddy-daddy-do.mp3";
+        String songPath2 = "/com/if2210/app/music/comedy.mp3";
+
+        songs.add(new Media(new File(getClass().getResource(songPath1).toURI()).toURI().toString()));
+        songs.add(new Media(new File(getClass().getResource(songPath2).toURI()).toURI().toString()));
+
+        mediaPlayer = new MediaPlayer(songs.get(currentSongIndex));
+        mediaPlayer.setOnEndOfMedia(() -> {
+            mediaPlayer.stop();
+            mediaPlayer = new MediaPlayer(songs.get((currentSongIndex + 1) % songs.size()));
+            mediaPlayer.play();
+            currentSongIndex = (currentSongIndex + 1) % songs.size();
+        });
+        mediaPlayer.play();
 
         stage.setTitle("MoliNana");
         stage.setScene(scene);
