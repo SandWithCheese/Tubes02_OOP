e04cd84d161f8f0132c1aa1c128057512973e784
Yusufarsan <sandiyusufardian@gmail.com>
2024-05-23 22:18:22 +0700

    feat: added pop up to choose file and filter the ext file

--- src/main/java/com/if2210/app/view/LoadPluginView.java
+++ src/main/java/com/if2210/app/view/LoadPluginView.java
@@ -1,13 +1,36 @@
 package com.if2210.app.view;
 
 import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import java.io.File;
 
 public class LoadPluginView {
     // attribut
+    @FXML
+    private Label fileNameLabel;
+
     // konstruktor
     // method
     @FXML
     public void clickChoosePlugin(){
-        
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Plugin");
+
+        // Add extension filter for .jar files
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("JAR files (*.jar)", "*.jar");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+
+        // Show file name to fileNameLabel
+        if (selectedFile != null) {
+            fileNameLabel.setText(selectedFile.getName());
+        }
+        else {
+            fileNameLabel.setText("No plugin selected");
+        }
+
     }
 }
