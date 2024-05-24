3f85ced909e78bfd9a1b87ee0736d71f1dbb326a
SandWithCheese <naufalahmad022@gmail.com>
2024-05-24 16:52:31 +0700

    fix: change view for active deck item in card info

--- src/main/java/com/if2210/app/view/CardInfoView.java
+++ src/main/java/com/if2210/app/view/CardInfoView.java
@@ -40,10 +40,10 @@ public class CardInfoView {
 
     @FXML
     private Label name;
-    
+
     @FXML
     private ImageView image;
-    
+
     @FXML
     private Label info;
 
@@ -56,7 +56,7 @@ public class CardInfoView {
     @FXML
     private Button harvestButton;
 
-    public CardInfoView(AnchorPane deck, GUIController gui){
+    public CardInfoView(AnchorPane deck, GUIController gui) {
         card = deck.getUserData();
         cardPane = deck;
         controller = gui;
@@ -68,7 +68,7 @@ public class CardInfoView {
         resProd.put("Beruang", "Daging Beruang");
     }
 
-    private ArrayList<String> mapToList(Map<String, Integer> map){
+    private ArrayList<String> mapToList(Map<String, Integer> map) {
         ArrayList<String> list = new ArrayList<>();
 
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
@@ -78,7 +78,7 @@ public class CardInfoView {
         return list;
     }
 
-    private Map<String, Integer> listToMap(ArrayList<String> list){
+    private Map<String, Integer> listToMap(ArrayList<String> list) {
         Map<String, Integer> map = new HashMap<>();
 
         for (String name : list) {
@@ -91,127 +91,130 @@ public class CardInfoView {
         return map;
     }
 
-    private void initInfoAnimal(){
-        weight.setText("Weight : "+ ((AnimalCardModel)card).getCurrentWeight());
-        harvestWeight.setText("Harvest weight : "+((AnimalCardModel)card).getHarvestWeight());
-        if(((AnimalCardModel)card).getCurrentWeight()>=((AnimalCardModel)card).getHarvestWeight()){
+    private void initInfoAnimal() {
+        AnimalCardModel animalCard = (AnimalCardModel) card;
+        int weightFromActiveItem = 0;
+        for (ItemCardModel item : animalCard.getActiveItems()) {
+            if (item.getName().equals("Accelerate")) {
+                weightFromActiveItem += 8;
+            } else if (item.getName().equals("Delay")) {
+                weightFromActiveItem -= 5;
+            }
+        }
+
+        if (weightFromActiveItem < 0) {
+            weightFromActiveItem = 0;
+        }
+        String weightText = "Weight : " + animalCard.getCurrentWeight() + " (" + weightFromActiveItem + ")";
+        weight.setText(weightText);
+        harvestWeight.setText("Harvest weight : " + animalCard.getHarvestWeight());
+        if ((animalCard.getCurrentWeight() + weightFromActiveItem) >= animalCard.getHarvestWeight()) {
             info.setText("Ready to be harvest");
             info.setTextFill(javafx.scene.paint.Color.GREEN);
             harvestButton.setVisible(true);
-        }else{
+        } else {
             info.setText("Your animal isn't ready to be harvested yet");
             info.setTextFill(javafx.scene.paint.Color.RED);
         }
 
-        // gunakan untuk testing
-        ArrayList<ItemCardModel> temp = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            temp.add(ItemCardFactory.createItemCard(ItemCardFactory.itemNames[1]));              
-        }
-        for (int i = 0; i < 3; i++) {
-            int random = (int) (Math.random() * 6);
-            temp.add(ItemCardFactory.createItemCard(ItemCardFactory.itemNames[random]));              
-        }
-        ((AnimalCardModel)card).setActiveItems(temp);
-        // -------------------------------
-
-        ArrayList<ItemCardModel> itemList = ((AnimalCardModel)card).getActiveItems();
+        ArrayList<ItemCardModel> itemList = animalCard.getActiveItems();
         ArrayList<String> nameItems = new ArrayList<>();
         for (ItemCardModel item : itemList) {
             nameItems.add(item.getName());
-            
         }
+
         Map<String, Integer> map = listToMap(nameItems);
         ArrayList<String> result = mapToList(map);
         System.out.println(itemList.size());
-        items.setText(result.toString());
+        items.setText(result.toString().substring(1, result.toString().length() - 1));
     }
 
-    public void initInfoPlant(){
-        weight.setText("Age : "+ ((PlantCardModel)card).getCurrentAge());
-        harvestWeight.setText("Harvest age : "+((PlantCardModel)card).getHarvestAge());
-        if(((PlantCardModel)card).getCurrentAge()>=((PlantCardModel)card).getHarvestAge()){
+    public void initInfoPlant() {
+        PlantCardModel plantCard = (PlantCardModel) card;
+        int ageFromActiveItem = 0;
+        for (ItemCardModel item : plantCard.getActiveItems()) {
+            if (item.getName().equals("Accelerate")) {
+                ageFromActiveItem += 2;
+            } else if (item.getName().equals("Delay")) {
+                ageFromActiveItem -= 2;
+            }
+        }
+
+        if (ageFromActiveItem < 0) {
+            ageFromActiveItem = 0;
+        }
+        String ageText = "Weight : " + plantCard.getCurrentAge() + " (" + ageFromActiveItem + ")";
+        weight.setText(ageText);
+
+        harvestWeight.setText("Harvest age : " + plantCard.getHarvestAge());
+        if ((plantCard.getCurrentAge() + ageFromActiveItem) >= plantCard.getHarvestAge()) {
             info.setText("Ready to be harvest");
             info.setTextFill(javafx.scene.paint.Color.GREEN);
-        }else{
+        } else {
             info.setText("Your plant isn't ready to be harvested yet");
             info.setTextFill(javafx.scene.paint.Color.RED);
         }
 
-        // gunakan untuk testing
-        ArrayList<ItemCardModel> temp = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            temp.add(ItemCardFactory.createItemCard(ItemCardFactory.itemNames[1]));              
-        }
-        for (int i = 0; i < 3; i++) {
-            int random = (int) (Math.random() * 6);
-            temp.add(ItemCardFactory.createItemCard(ItemCardFactory.itemNames[random]));              
-        }
-        ((PlantCardModel)card).setActiveItems(temp);
-        // -------------------------------
-
-        ArrayList<ItemCardModel> itemList = ((PlantCardModel)card).getActiveItems();
+        ArrayList<ItemCardModel> itemList = plantCard.getActiveItems();
         ArrayList<String> nameItems = new ArrayList<>();
         for (ItemCardModel item : itemList) {
             nameItems.add(item.getName());
-            
         }
         Map<String, Integer> map = listToMap(nameItems);
         ArrayList<String> result = mapToList(map);
         System.out.println(itemList.size());
-        items.setText(result.toString());
+        items.setText(result.toString().substring(1, result.toString().length() - 1));
     }
 
-    public void initInfoProduct(){
-        weight.setText("Price : "+ ((ProductCardModel)card).getPrice());
-        harvestWeight.setText("Additional weight : "+((ProductCardModel)card).getAddedWeight());
+    public void initInfoProduct() {
+        weight.setText("Price : " + ((ProductCardModel) card).getPrice());
+        harvestWeight.setText("Additional weight : " + ((ProductCardModel) card).getAddedWeight());
     }
 
-    public void initialize(){
+    public void initialize() {
         harvestButton.setVisible(false);
-        name.setText(((CardModel)card).getName());
+        name.setText(((CardModel) card).getName());
         System.out.println(card.getClass().getName());
-        if(card instanceof AnimalCardModel){
+        if (card instanceof AnimalCardModel) {
             initInfoAnimal();
-        }else if(card instanceof PlantCardModel){
+        } else if (card instanceof PlantCardModel) {
             initInfoPlant();
-        }else{
+        } else {
             initInfoProduct();
         }
-        Image img = new Image(((CardModel)card).getImage());
+        Image img = new Image(((CardModel) card).getImage());
         image.setImage(img);
 
     }
 
-    private void harvestPlant(){
+    private void harvestPlant() {
         System.out.println("Menjalankan proses panen tanaman");
     }
-    
-    private void harvestAnimal(){
-        System.out.println("Menjalankan proses panen hewan");
-        ProductCardModel produk = ProductCardFactory.createProductCard(resProd.get(((CardModel)card).getName()));
-        controller.updateCard(cardPane, produk);
-        Stage stage = (Stage)info.getScene().getWindow();
-        stage.close();
 
+    private void harvestAnimal() {
+        System.out.println("Menjalankan proses panen hewan");
+        ProductCardModel produk = ProductCardFactory.createProductCard(resProd.get(((CardModel) card).getName()));
+        controller.updateCard(cardPane, produk);
+        Stage stage = (Stage) info.getScene().getWindow();
+        stage.close();
     }
 
-    public void harvestHandler(){
-        if(card instanceof AnimalCardModel){
-            if(((AnimalCardModel)card).getCurrentWeight()>=((AnimalCardModel)card).getHarvestWeight()){
+    public void harvestHandler() {
+        if (card instanceof AnimalCardModel) {
+            if (((AnimalCardModel) card).getCurrentWeight() >= ((AnimalCardModel) card).getHarvestWeight()) {
                 harvestAnimal();
-            }else{
+            } else {
                 harvestResponse.setText("The animal is not heavy enough to harvest");
                 harvestResponse.setTextFill(javafx.scene.paint.Color.RED);
             }
-        }else if(card instanceof PlantCardModel){
-            if(((PlantCardModel)card).getCurrentAge()>=((PlantCardModel)card).getHarvestAge()){
+        } else if (card instanceof PlantCardModel) {
+            if (((PlantCardModel) card).getCurrentAge() >= ((PlantCardModel) card).getHarvestAge()) {
                 harvestPlant();
-            }else{
+            } else {
                 harvestResponse.setText("The plants are not yet mature enough to harvest");
                 harvestResponse.setTextFill(javafx.scene.paint.Color.RED);
             }
-        }else{
+        } else {
             harvestResponse.setText("Product is not a harvestable entity");
             harvestResponse.setTextFill(javafx.scene.paint.Color.RED);
         }
